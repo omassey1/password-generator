a1b22fc47eb88bec49c496fd0ac4ff7e23ba9167
omassey1 <65238159+omassey1@users.noreply.github.com>
2022-11-29 09:25:15 -0500

    added conditionals

--- script.js
+++ script.js
@@ -8,14 +8,33 @@ function generatePassword() {
   var upLetters = letters.toUpperCase();
   var chooseFrom = "";
   //CODE GOES HERE
-  var isUppercase = confirm('Would you like to use uppercase?');
-  var isLowercase = false;
-  var isSpecial = false;
-  var isNumber = false;
+  var isUppercase = confirm('Would you like to use uppercase letters?');
+  var isLowercase = confirm('Would you like to use lowercase letters?');
+  var isSpecial = confirm('Would you like to use special characters?');
+  var isNumber = confirm('Would you like to use numbers?');
 
   if (isUppercase) {
-    console.log('uppercase')
+    console.log('uppercase');
+    chooseFrom += upLetters;
   }
+
+  if (isLowercase) {
+    console.log('lowercase');
+    chooseFrom += letters;
+  }
+
+  if (isSpecial) {
+    console.log('special');
+    chooseFrom += special
+  }
+  if (isNumber) {
+    console.log('numbers')
+    chooseFrom += numbers
+  }
+
+  console.log(chooseFrom);
+
+  return "";
 }
 
 
