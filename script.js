2d541e67c19d530a44fb86421df1069e206f465c
omassey1 <65238159+omassey1@users.noreply.github.com>
2022-11-30 19:48:47 -0500

    variables

--- script.js
+++ script.js
@@ -2,40 +2,17 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var special = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
-  var letters = "abcdefghijklmnopqrstuvwxyz";
-  var numbers = "0987654321";
-  var upLetters = letters.toUpperCase();
-  var chooseFrom = "";
-  //CODE GOES HERE
-  var isUppercase = confirm('Would you like to use uppercase letters?');
-  var isLowercase = confirm('Would you like to use lowercase letters?');
-  var isSpecial = confirm('Would you like to use special characters?');
-  var isNumber = confirm('Would you like to use numbers?');
-
-  if (isUppercase) {
-    console.log('uppercase');
-    chooseFrom += upLetters;
+  var length = window.prompt("choose password length between 8 and 128")
+char = "abcdefghijklmnopqrstuvwkyz"
+num = "1234567890"
+passwordText = [""]
+ for(i=0;i < length; i++){
+   passwordText += char.charAt(Math.floor(Math.random() * char.length))
+   return passwordText
   }
+};
 
-  if (isLowercase) {
-    console.log('lowercase');
-    chooseFrom += letters;
-  }
 
-  if (isSpecial) {
-    console.log('special');
-    chooseFrom += special
-  }
-  if (isNumber) {
-    console.log('numbers')
-    chooseFrom += numbers
-  }
-
-  console.log(chooseFrom);
-
-  return "";
-}
 
 
 
